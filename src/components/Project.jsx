a223c5d3030f8b304eff525529515cccb60a3bbc
JS <apple@Joons-MacBook-Air.local>
2025-09-11 08:41:32 -0700

    Updated Project

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -2,7 +2,7 @@ const Project = ({project}) => {
   return (
     <div className="project">
       <div className="title">
-        <a href="https://weather-website-wheat.vercel.app/">{project.title}</a>
+        <a href={project.url}>{project.title}</a>
       </div>
       <div className="subtext">
         {project.subtext}
