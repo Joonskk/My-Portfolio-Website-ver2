6e964fb313003b21073e3e02413ace9fb2ec1092
JS <apple@Joons-MacBook-Air.local>
2025-09-11 08:41:03 -0700

    Updated Info

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -8,17 +8,11 @@ const Info = () => {
         <a href="https://github.com/Joonskk">Github<MdArrowOutward /></a>
         <a href="https://www.linkedin.com/in/joonseok-kim-a23251267?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app">LinkedIn<MdArrowOutward /></a>
       </div>
-      <div className="subwebsite">
-        <a href="">experiments.jskim.me<MdArrowOutward /></a>
-        <a href="">playground.jskim.me<MdArrowOutward /></a>
-      </div>
       <div className="awards">
         <h1>Awwwards Independent of the Year</h1>
-        <h2>2020 Nominee</h2>
-        <h1>Awwwards Site of the Day</h1>
-        <h2>Web Graphic Experiments</h2>
-        <h1>Awwwards Site of the Day</h1>
-        <h2>100 DAYS OF POETRY</h2>
+        <h2>2025 Python Coding Contest</h2>
+        <h1>Awwwards from outside activities</h1>
+        <h2>High School Math Competition</h2>
       </div>
     </div>
   )
